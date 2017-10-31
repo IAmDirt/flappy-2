92a0c6ed64ef9abaab26f563b4903848d68e06d6
cecixy <cecibasket12@hotmail.com>
2017-10-31 15:49:23 +0100

    Removed excess slashes again and added flappy bird basic unity project
    Removed excess slashes (again) and

---
+++ 2D Flappy Bird/Assets/Scripts/SoundManagerScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour {
+
+	public static AudioClip birdHitSound, birdFlapSound;
+	static AudioSource audioSrc;
+
+	// Use this for initialization
+	void Start () {
+		
+		birdHitSound = Resources.Load<AudioClip> ("birdHit");
+		birdFlapSound = Resources.Load<AudioClip> ("birdFlap");
+
+		audioSrc = GetComponent<AudioSource> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static void PlaySound (string clip)
+	{
+		switch (clip) {
+		case "birdHit":
+			audioSrc.PlayOneShot (birdHitSound);
+			break;
+		}
+		
+	switch (clip) {
+		case "birdFlap":
+			audioSrc.PlayOneShot (birdFlapSound);
+			break;
+
+		}
+	}
+}
