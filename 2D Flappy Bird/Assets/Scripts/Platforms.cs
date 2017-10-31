d0dfb1a1b00ebb62865599412ca7f05612433541
cecixy <cecibasket12@hotmail.com>
2017-10-31 16:54:51 +0100

    Added Platforms Script (and unparented some BG stuff)

---
+++ 2D Flappy Bird/Assets/Scripts/Platforms.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforms : MonoBehaviour {
+
+
+
+	void Start() 
+	{
+		RandomMapBuilder();
+	}
+
+
+	void RandomMapBuilder()
+	{
+		for (int i = 0; i < 20; i++) {
+			Vector3 SpawnPosition = new Vector3 (21, Random.Range (-3, 2), 0);
+			Instantiate (GameObject.Find("Ground"), SpawnPosition, Quaternion.identity); 
+		}
+	}
+}
