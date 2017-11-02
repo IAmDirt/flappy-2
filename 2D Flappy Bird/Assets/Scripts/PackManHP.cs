286078942b008045e78f25bca9659c8fca3d2db7
IAmDirt <jbalevik@gmail.com>
2017-11-02 18:19:43 +0100

    2prøv å fikse

---
+++ 2D Flappy Bird/Assets/Scripts/PackManHP.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackManHP : MonoBehaviour {
+	private Animator anim;
+
+	private float PackManLiv = 3;
+
+	public GameObject DamageHit;
+
+	// Use this for initialization
+	void Start () {
+
+
+		anim = GetComponent<Animator> ();
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+	IEnumerator vent()
+	{
+		yield return new WaitForSeconds (0.7f);
+
+		Destroy(gameObject);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "droplet") 
+		{
+
+			if (PackManLiv < 1) 
+			{
+
+				anim.SetTrigger ("PacManDie 0");
+				StartCoroutine (vent ());
+			}
+
+			Debug.Log ("dø pack man");
+			PackManLiv--;
+			Instantiate (DamageHit, transform.position, Quaternion.identity);
+
+
+
+		}
+}
+
+}
