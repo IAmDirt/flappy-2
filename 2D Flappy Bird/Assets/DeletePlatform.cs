a904114697b22b67759bad34a80b587ab8092029
cecixy <cecibasket12@hotmail.com>
2017-11-02 16:32:52 +0100

    something about platforms

--- 2D Flappy Bird/Assets/DeletePlatform.cs
+++ 2D Flappy Bird/Assets/DeletePlatform.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class DeletePlatform : MonoBehaviour {
 
+
 	void OnCollisionEnter2D(Collision2D coll)
 	{
 
 		if (coll.gameObject.tag == "platform") 
 		{
+			Debug.Log ("ICE CREAM");
 			Destroy(gameObject);
 		}
 	}
