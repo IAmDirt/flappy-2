853b867106d3a99e63995f20aeddb940b6c2db36
cecixy <cecibasket12@hotmail.com>
2017-11-02 20:51:44 +0100

    Destroying bird when colliding with destroyer

---
+++ 2D Flappy Bird/Assets/Scripts/DestroyPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPlayer : MonoBehaviour {
+
+	public GameObject Bird; 
+
+	void OnCollisionStay2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Player") {
+			Debug.Log ("I CRASH I KILL"); 
+			Destroy (Bird);
+		}
+	}
+}
